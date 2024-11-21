dc5146b4579b3cceaf9c9e63b89a75619837fdc8
Fajri Rinaldi Chan <fajri@gariskode.com>
2024-11-22 02:55:47 +0700

    fix

--- resources/views/livewire/exam/show.blade.php
+++ resources/views/livewire/exam/show.blade.php
@@ -1,4 +1,4 @@
-<div class="app-page  flex-column flex-column-fluid " id="kt_app_page">
+<div class="app-page  flex-column flex-column-fluid " id="kt_app_page" data-kt-drawer-dismiss="true">
     @include('exam/layout/partials/_header')
     <!--begin::Wrapper-->
     <div class="app-wrapper  flex-column flex-row-fluid " id="kt_app_wrapper">
