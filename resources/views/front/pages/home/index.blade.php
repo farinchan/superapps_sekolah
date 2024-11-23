79ef992d476a3e9dbc0aa3dd4db0089be8d9a54c
Fajri Rinaldi Chan <fajri@gariskode.com>
2024-11-23 12:17:25 +0700

    update

--- resources/views/front/pages/home/index.blade.php
+++ resources/views/front/pages/home/index.blade.php
@@ -570,7 +570,7 @@
                                         </div>
                                         <div class="teacher-img-category">
                                             <div class="teacher-img">
-                                                <img src="{{ $teacher->getPhoto() }}" alt="">
+                                                <img src="{{ $teacher->getPhoto() }}" alt="" style="height: 170px; width: 200; object-fit: cover;">
                                                 <div class="course-price text-uppercase text-center gradient-bg">
                                                     <span>Featured</span>
                                                 </div>
