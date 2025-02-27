850f95a506c8c36bd74f52f0f0d2f308080f6464
Fajri Rinaldi Chan <fajri@gariskode.com>
2025-02-27 08:05:54 +0700

    fix: icon ppdbexam for menu side

--- resources/views/back/layout/partials/sidebar/_menu.blade.php
+++ resources/views/back/layout/partials/sidebar/_menu.blade.php
@@ -726,12 +726,9 @@
                         <a class="menu-link @if (request()->routeIs('back.ppdb.exam.exam') || request()->routeIs('back.ppdb.student.*')) active @endif"
                             href="{{ route('back.ppdb.exam.exam') }}">
                             <span class="menu-icon">
-                                <i class="ki-duotone ki-people fs-2">
+                                <i class="ki-duotone ki-tablet-book fs-2">
                                     <span class="path1"></span>
                                     <span class="path2"></span>
-                                    <span class="path3"></span>
-                                    <span class="path4"></span>
-                                    <span class="path5"></span>
                                 </i>
                             </span>
                             <span class="menu-title">Ujian</span>
